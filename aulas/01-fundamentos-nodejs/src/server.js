b43dfd007facc82cdfa2675cef5c7fafe067b9ac
Marcos Vinícius Silva <marcoscv71@gmail.com>
2025-02-27 09:55:16 -0300

    criando o arquivo estudo e modificando as alteraçoes no server.js

--- aulas/01-fundamentos-nodejs/src/server.js
+++ aulas/01-fundamentos-nodejs/src/server.js
@@ -1,14 +1,6 @@
-// const http = require('http');
+import http from 'node:http';
 
-// CommonJS => require
-// ESModules => import/export
-// import fastfy from 'fastfy';
-
-
-import http from 'node:http'; // forma de importar modulos que sao nativos do node
-
-// Criar um usuário (name, email, password) -> o req eu consigo pegar rodas as informações que
-// envio ao servidor
+const users = []
 
 const server = http.createServer((req, res) => {
     const {method, url} = req
@@ -18,28 +10,15 @@ const server = http.createServer((req, res) => {
     }
 
     if(method === 'POST' && url === '/users') {
+        users.push({
+            id: 1,
+            name: 'John Doe',
+            email: 'johndoe@example.com'
+        })
         return res.end('Criação de usuário')
     }
 
     return res.end('Hello World')
 })
 
-server.listen(3333) // localhost:3333
-
-// Rotas são caminhos de entrada dentro da nossa aplicação
-// Formas do front end executas diferentes operações dentro do backend
-// Criar usuarios
-// Listagem usuarios
-// Edição de usuarios
-// Remoção de usuarios
-
-// - HTTP 
-//    - Método HTTP
-//    - URL
-// GET, POST, PUT, PATCH, DELETE
-
-// GET => buscar um recurso do beckend
-// POST => Criar um recurso
-// PUT => Editar ou atualizar um recurso 
-// PATCH => Atualizar uma informação específica de um recurso
-// DELETE => deletar um recurso no backend
+server.listen(3333) 
